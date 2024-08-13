97b22fb49498b03969be539ee888d7ff60430300
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-08-12 22:27:11 -0300

    Second Commit ~ List2 TP Running

--- haskell-exercises/lists2/src/ListsPart2.hs
+++ haskell-exercises/lists2/src/ListsPart2.hs
@@ -10,13 +10,21 @@ bitAt :: Int -> Char -> Bit
 bitAt n c = if testBit (ord c) (7-n) then T else F 
 
 charToBits :: Char -> Bits
-charToBits = error "Implement It"
-
+charToBits c = [bitAt n c | n <- [0..7]]
 
 bits::String -> Bits
-bits = error "Implement It"
+bits = foldl (\acc c -> acc ++ charToBits c) []
 
 type Solution = [Int]
 
 queens::Int -> [Solution]
-queens error "Implement It"
+queens n = solve n
+  where
+    solve 0 = [[]]  -- Base case: one solution for a 0x0 board (empty board)
+    solve k = [q:qs | qs <- solve (k-1), q <- [1..n], safe q qs 1]
+
+    -- Check if placing a queen in column q is safe from attacks
+    safe :: Int -> [Int] -> Int -> Bool
+    safe _ [] _ = True
+    safe q (q':qs) dist =
+      q /= q' && q /= q' + dist && q /= q' - dist && safe q qs (dist + 1)
