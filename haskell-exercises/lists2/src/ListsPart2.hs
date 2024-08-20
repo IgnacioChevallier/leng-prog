8589fb7dab65ce9302e8ad38d16bdfc6670ac776
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-08-19 22:43:58 -0300

    Commit Trie

--- haskell-exercises/lists2/src/ListsPart2.hs
+++ haskell-exercises/lists2/src/ListsPart2.hs
@@ -17,14 +17,11 @@ bits = foldl (\acc c -> acc ++ charToBits c) []
 
 type Solution = [Int]
 
-queens::Int -> [Solution]
-queens n = solve n
+queens :: Int -> [Solution]
+queens size = solve 1 [[]]
   where
-    solve 0 = [[]]  -- Base case: one solution for a 0x0 board (empty board)
-    solve k = [q:qs | qs <- solve (k-1), q <- [1..n], safe q qs 1]
-
-    -- Check if placing a queen in column q is safe from attacks
-    safe :: Int -> [Int] -> Int -> Bool
-    safe _ [] _ = True
-    safe q (q':qs) dist =
-      q /= q' && q /= q' + dist && q /= q' - dist && safe q qs (dist + 1)
+    solve i solutions
+      | i > size = solutions
+      | otherwise = solve (i + 1) [j : solution | j <- [1 .. size], solution <- solutions, isSafe j 1 solution]
+    isSafe _ _ [] = True
+    isSafe j offset (c : cs) = j /= c && j /= c - offset && j /= c + offset && isSafe j (offset + 1) cs
--- haskell-exercises/trie/src/Trie.hs
+++ haskell-exercises/trie/src/Trie.hs
@@ -1,20 +1,45 @@
 module Trie  (Trie(..), left, right, find, decode, toList) where
 
 import Bit
-  
-data Trie a = Complete -- Add the Type definition deriving (Eq, Show)
-            
+
+data Trie a = Nil | Leaf a | (Trie a) :-: (Trie a)
+            deriving (Eq, Show)
+
 left::Trie a -> Trie a
-left = error "Define it"
+left trie = case trie of
+  Nil -> Nil
+  (Leaf _) -> Nil
+  (l :-: _) -> l
 
 right::Trie a -> Trie a
-left = error "Define it"
-  
-find::Bits -> Trie a -> a
-find = error "Define it"
+right trie = case trie of
+    Nil -> Nil
+    (Leaf _) -> Nil
+    (_ :-: r) -> r
 
-decode::Bits -> Trie Char -> String
-decode = error "Define it"
+find :: Bits -> Trie a -> a
+find [] (Leaf a) = a
+find [] _ = error "Not found"
+find (x:xs) (l :-: r) = if x == F then find xs l else find xs r
+find _ _ = error "Invalid pattern"
+
+decode :: Bits -> Trie Char -> String
+decode [] _ = ""
+decode bits trie = let (prefix, rest) = splitPrefix bits trie
+                   in find prefix trie : decode rest trie
+
+-- Helper function to split the bits into a prefix that leads to a leaf and the rest
+splitPrefix :: Bits -> Trie a -> (Bits, Bits)
+splitPrefix [] _ = ([], [])
+splitPrefix bits@(x:xs) node = case node of
+  Leaf _ -> ([], bits)
+  l :-: r -> if x == F
+             then let (prefix, rest) = splitPrefix xs l in (x:prefix, rest)
+             else let (prefix, rest) = splitPrefix xs r in (x:prefix, rest)
+  _ -> ([], bits)
 
 toList::Trie a -> [(a, Bits)]
-toList = error "Define it"
+toList Nil = []
+toList (Leaf a) = [(a, [])]
+toList (l :-: r) = map (addBit F) (toList l) ++ map (addBit T) (toList r)
+  where addBit b (a, bits) = (a, b:bits)
