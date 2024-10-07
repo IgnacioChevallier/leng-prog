a26fd1005346cc40d52a9fcbe938a1809d01bb44
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-10-07 17:17:54 -0300

    TP 9 DONE

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,23 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+    let mut multiples = HashSet::new();
+    for factor in factors {
+        let mut n = 1;
+        while factor * n < limit {
+            multiples.insert(factor * n);
+            if *factor == 0 { break }
+            n += 1;
+        }
+    }
+    println!("{:?}", multiples);
+    get_sum_of_list(&multiples)
+}
+
+pub fn get_sum_of_list(list: &HashSet<u32>) -> u32 {
+    let mut sum = 0;
+    for i in list {
+        sum += i;
+    }
+    sum
 }
--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -1,6 +1,17 @@
 use std::collections::HashMap;
 
-/// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+    let words: Vec<String> = get_words_lowercased(phrase);
+    let mut word_count = HashMap::new();
+
+    for word in words {
+        let count = word_count.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    word_count
+}
+
+pub fn get_words_lowercased(phrase: &str) -> Vec<String> {
+    phrase.split_whitespace().map(|word| word.to_lowercase()).collect()
 }
